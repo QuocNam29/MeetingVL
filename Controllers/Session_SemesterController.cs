306028ff1be9b932e84ace627dfaed76a0ec2e89
197PM33932 - TRẦN QUỐC NAM - K25T-PM1 <nam.197pm33932@vanlanguni.vn>
2022-07-19 13:48:33 +0700

    tạo view xem đánh giá

---
+++ Controllers/Session_SemesterController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MeetingVL.Models;
+
+namespace MeetingVL.Controllers
+{
+    public class Session_SemesterController : Controller
+    {
+        private SEP25Team13Entities db = new SEP25Team13Entities();
+
+        // GET: Session_Semester
+        public ActionResult Index()
+        {
+            var session_Semester = db.Session_Semester.Include(s => s.Semester).Include(s => s.SessionReport);
+            return View(session_Semester.ToList());
+        }
+
+        // GET: Session_Semester/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Session_Semester session_Semester = db.Session_Semester.Find(id);
+            if (session_Semester == null)
+            {
+                return HttpNotFound();
+            }
+            return View(session_Semester);
+        }
+
+        // GET: Session_Semester/Create
+        public ActionResult Create()
+        {
+            ViewBag.Semester_ID = new SelectList(db.Semesters, "ID", "User_ID");
+            ViewBag.SessionReport_ID = new SelectList(db.SessionReports, "ID", "Name");
+            return View();
+        }
+
+        // POST: Session_Semester/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,SessionReport_ID,Semester_ID")] Session_Semester session_Semester)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Session_Semester.Add(session_Semester);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Semester_ID = new SelectList(db.Semesters, "ID", "User_ID", session_Semester.Semester_ID);
+            ViewBag.SessionReport_ID = new SelectList(db.SessionReports, "ID", "Name", session_Semester.SessionReport_ID);
+            return View(session_Semester);
+        }
+
+        // GET: Session_Semester/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Session_Semester session_Semester = db.Session_Semester.Find(id);
+            if (session_Semester == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Semester_ID = new SelectList(db.Semesters, "ID", "User_ID", session_Semester.Semester_ID);
+            ViewBag.SessionReport_ID = new SelectList(db.SessionReports, "ID", "Name", session_Semester.SessionReport_ID);
+            return View(session_Semester);
+        }
+
+        // POST: Session_Semester/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,SessionReport_ID,Semester_ID")] Session_Semester session_Semester)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(session_Semester).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Semester_ID = new SelectList(db.Semesters, "ID", "User_ID", session_Semester.Semester_ID);
+            ViewBag.SessionReport_ID = new SelectList(db.SessionReports, "ID", "Name", session_Semester.SessionReport_ID);
+            return View(session_Semester);
+        }
+
+        // GET: Session_Semester/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Session_Semester session_Semester = db.Session_Semester.Find(id);
+            if (session_Semester == null)
+            {
+                return HttpNotFound();
+            }
+            return View(session_Semester);
+        }
+
+        // POST: Session_Semester/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Session_Semester session_Semester = db.Session_Semester.Find(id);
+            db.Session_Semester.Remove(session_Semester);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
